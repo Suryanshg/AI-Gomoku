650895db48a4cb846ef6027d87da0ed420b8cb06
kgandhi09 <kushal009gandhi@gmail.com>
2020-10-05 05:00:31 -0400

    diagonal evaluation almost done

--- board.py
+++ board.py
@@ -151,22 +151,7 @@ def generate_and_place_random(ourTeam):
 def int_to_letter(col):
     return chr(col + charOffset)
 
-# Board for testing the Eval function
-test_board = [[1, 2, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 2, 2], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0],
-              [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 2, 0], 
-              [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0], 
-              [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0], 
-              [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0], 
-              [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
 # Returns the eval value for rows
 def row_eval(board, team):
     board_size = 15
@@ -330,8 +315,160 @@ def col_eval(board, team):
 
     return col_eval
 
+# Board for testing the Eval function
+test_board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]]
 
+def diag_eval(board, team):
+    board_size = 15
 
+    # Dictionary that takes into account [NumOfPieces : [WithZeroOpenedPlaces, WithOneOpenedPlaces, WithTwoOpenedPlaces]]
+    board_count = {1:[0,0,0], 2:[0,0,0], 3:[0,0,0], 4:[0,0,0]}
+
+    for y in range(board_size):  
+        for x in range(board_size):
+            consec = 0 # Track of Consecutives in a row
+            open_ends = 0 # Track of respective opened space with consecutives
+            if board[y][x] == team:
+                if x > 0 and y > 0: # Check for the boundary conditions
+                    if board[y-1][x-1] == 0: # If empty, means an opened space
+                        open_ends += 1
+                consec += 1
+
+                i = 1           
+
+                # Lets check for the consecutives after the current piece
+                while True:
+                    if x+i < board_size and y + i < board_size:
+                        if board[y+i][x+i] == team: # Increase the consecutives
+                            consec += 1
+                        elif board[y+i][x+i] != team: # Check for opened spaces and stop
+                            if board[y+i][x+i] == 0:
+                                open_ends += 1
+                            break
+                    elif x+i == board_size or y+i == board_size: # Stops at the boundary condition
+                        break  
+                    i += 1
+
+                if 0 < consec < 5 and open_ends > 0: # If not the winning condition in a row
+                    board_count[consec][open_ends] += 1 # Update the dictionary
+                elif consec == 5: # If winning condition in a row
+                    return 1000000
+                print(x)
+                print(board_count)
+
+                # Resetting the consecutive count and the opened spaces count before going backwards
+                consec = 1
+                open_ends = 0
+
+                if x < board_size - 1 and y < board_size -1: # Check for the boundary conditions
+                    if board[y+1][x+1] == 0: # If empty, means an opened space
+                        open_ends += 1
+
+                j = 1
+
+                # Lets check for the consecutives before the current piece
+                while True:
+                    if x-j >= 0 and y- j >= 0:
+                        if board[y-j][x-j] == team:  # Increase the consecutives
+                            consec += 1
+                        elif board[y-j][x-j] != team: # Check for opened spaces and stop
+                            if board[y-j][x-j] == 0:
+                                open_ends += 1
+                            break
+                    elif x-j < 0 and y-j < 0: # Stops at the boundary condition
+                        break  
+                    j += 1
+                
+                if 0 < consec < 5 and open_ends > 0:  # If not the winning condition in a row
+                    board_count[consec][open_ends] += 1 # Update the dictionary
+                elif consec == 5: # If winning condition in a row
+                    return 1000000
+                print(board_count)
+
+    #_--------------------------------------------------------------------------------------------------------------------------
+                consec = 1
+                open_ends = 0
+
+                if x > 0 and y < board_size - 1: # Check for the boundary conditions
+                    if board[y+1][x-1] == 0: # If empty, means an opened space
+                        open_ends += 1
+               
+
+                k = 1           
+
+                # Lets check for the consecutives after the current piece
+                while True:
+                    if x+k < board_size - 1 and y - k > 0:
+                        if board[y-k][x+k] == team: # Increase the consecutives
+                            consec += 1
+                        elif board[y-k][x+k] != team: # Check for opened spaces and stop
+                            if board[y-k][x+k] == 0:
+                                open_ends += 1
+                            break
+                    elif x+k == board_size or y-k == 0: # Stops at the boundary condition
+                        break  
+                    k += 1
+
+                if 0 < consec < 5 and open_ends > 0: # If not the winning condition in a row
+                    board_count[consec][open_ends] += 1 # Update the dictionary
+                elif consec == 5: # If winning condition in a row
+                    return 1000000
+                
+                print(board_count)
+
+                # Resetting the consecutive count and the opened spaces count before going backwards
+                consec = 1
+                open_ends = 0
+
+                if x < board_size - 1 and y > 0: # Check for the boundary conditions
+                    if board[y-1][x+1] == 0: # If empty, means an opened space
+                        open_ends += 1
+
+                j = 1
+
+                # Lets check for the consecutives before the current piece
+                while True:
+                    if x-j > 0 and y + j < board_size - 1:
+                        if board[y+j][x-j] == team:  # Increase the consecutives
+                            consec += 1
+                        elif board[y+j][x-j] != team: # Check for opened spaces and stop
+                            if board[y+j][x-j] == 0:
+                                open_ends += 1
+                            break
+                    elif x-j < 0 or y+j > board_size: # Stops at the boundary condition
+                        break  
+                    j += 1
+                
+                if 0 < consec < 5 and open_ends > 0:  # If not the winning condition in a row
+                    board_count[consec][open_ends] += 1 # Update the dictionary
+                elif consec == 5: # If winning condition in a row
+                    return 1000000
+                print(board_count)
+
+            print("-------------------------------------------")
+    # Lets evaluate on the basis of data about consecutive pieces and opened spaces
+    print(board_count)
+    diag_eval = 0.0
+    for consecs in board_count:
+        for i in range(len(board_count[consecs])):
+            w = assign_weights(consecs, i) # Acquire the respective weights
+            diag_eval += board_count[consecs][i] * w # Calculate the weighted sum
+
+    return diag_eval
 
 # Assigns weights to different cases of consecutive pieces and opened nodes in them, 
 # l -- The number of consecutive pieces
@@ -376,5 +513,5 @@ def main():
     
 #main()
 
-test = col_eval(test_board,2)
+test = diag_eval(test_board,1)
 print(test)
