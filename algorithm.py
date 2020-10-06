ebd3e54882799103b84b71b6fe85f69074805e09
Brian-Earl <43723279+Brian-Earl@users.noreply.github.com>
2020-10-05 22:04:25 -0400

    Updated Min Max with depth limits, and timouts
    min_max_alpha_beta() now must also take in a depth limit else there will be no limit and the search wil go to the bottom of the tree. Additionally a timeout function has been implemented where min_max_alpha_beta() will return the best path it was able to find after a certain amount of seconds has passed. The amount of seconds until timeout is stored in the constant TIMEOUTAMOUNT as an int.

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,19 @@
 from random import randint
+import time
+import sys
+from threading import Thread
 
 #A single node in a tree of utility nodes
 #   each node contains:
 #   The utility of the current node
 #   A reference to the parent of the current node
 #   A list of children to the current node
+#   A variable that holds the move
 class Node:
-    def __init__(self, utility=0, parent=None):
-        self.utility = utility    
+    def __init__(self, utility=0, parent=None, move=(0,0)):
+        self.utility = utility
         self.parent = parent  
+        self.move = move
         self.children = []
 
     def add_child(self, child):
@@ -17,31 +22,65 @@ class Node:
 #Constant variable for storing positive infinity
 INF = float('inf')
 
+#Constant variable for the utility of five pieces in a row
+FIVEROWPOS = 100000
+FIVEROWNEG = -100000
+
+#Constant variable for how many seconds can pass before a forced time out should happen 
+TIMEOUTAMOUNT = 9
+
+#Variables used in min_max_alpha_beta(), they need to be outside the function to fix error due to encapsulation
+bestPath = None
+alpha = -INF
+beta = INF
+
 #Main function for finding the best path to follow using Min Max with Alpha Beta Pruning
 #   Input a tree of Nodes that you wish to search through
-#   Function will output the best child Node from the root to follow#Main function for finding the best path to follow using Min Max with Alpha Beta Pruning
-def min_max_alpha_beta(node):
-    bestPath = None
-    alpha = -INF
-    beta = INF
-    children = node.children
-    for child in children:
-        bestValue = min_value(child,alpha,beta)
+#   Function will output the best child Node from the root to follow
+def min_max_alpha_beta(node, depthLimit = -1):
+    global alpha
+    global beta
+    global bestPath
+
+    #Function for searching through list and setting the new alpha and bestPath
+    #   This exists as its own function as to allow for threading and therefore timeouts 
+    def mmab_helper(child):
+        global alpha
+        global beta
+        global bestPath
+
+        bestValue = min_value(child,alpha,beta,0,depthLimit)
         if bestValue > alpha:
             alpha = bestValue
             bestPath = child
+
+    bestPath = None
+    alpha = -INF
+    beta = INF
+
+    #Threading the search to allow for timeouts after a certain number of seconds
+    threads = []
+    try:
+        for child in node.children:
+            t = Thread(target=mmab_helper(child))
+            t.start()
+            threads.append(t)
+            time.sleep(TIMEOUTAMOUNT)
+    finally:
+        [t.join() for t in threads]
+
     return bestPath
 
 #Finds the child node with the highest utility while also alpha beta pruning
 #   When the bottom of a tree is found, the utility of the Node is returned
 #   Then the function works its way back up finding the best one using Min Max 
-def max_value(node, alpha, beta):
-    if len(node.children) == 0:
+def max_value(node, alpha, beta, depth, limit):
+    if len(node.children) == 0 or (limit>0 and depth>=limit):
         return node.utility
     maxValue = -INF
     children = node.children
     for child in children:
-        maxValue = max(maxValue, min_value(child,alpha, beta))
+        maxValue = max(maxValue, min_value(child,alpha,beta,depth+1,limit))
         if maxValue >= beta:
             return maxValue
         alpha = max(alpha,maxValue)
@@ -50,13 +89,13 @@ def max_value(node, alpha, beta):
 #Finds the child node with the lowest utility while also alpha beta pruning
 #   When the bottom of a tree is found, the utility of the Node is returned
 #   Then the function works its way back up finding the best one using Min Max 
-def min_value(node, alpha, beta):
-    if len(node.children) == 0:
+def min_value(node, alpha, beta, depth, limit):
+    if len(node.children) == 0 or (limit>0 and depth>=limit):
         return node
     minValue = INF
     children = node.children
     for child in children:
-        minValue = min(minValue, max_value(child,alpha,beta))
+        minValue = min(minValue, max_value(child,alpha,beta,depth+1,limit))
         if minValue <= alpha:
             return minValue
         beta = min(beta,minValue)
@@ -75,7 +114,7 @@ def min_max_test():
     for x in range(3):
         node = Node(-INF,parent)
         for y in range(3):
-            randomNum = randint(0,12)
+            randomNum = randint(FIVEROWNEG,FIVEROWPOS)
             print(randomNum)
             node.add_child(Node(randomNum,node))
         parent.add_child(node)
